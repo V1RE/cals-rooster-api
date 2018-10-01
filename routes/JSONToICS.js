6bdb8efc86ef5a920b7e407a1140ceeaaeb5c87e
Niels Mentink <niels.mentink@outlook.com>
2018-10-01 22:28:25 +0000

    Added comments to clarify code

--- routes/JSONToICS.js
+++ routes/JSONToICS.js
@@ -1,12 +1,15 @@
+// Adding 'ics' package for exporting JSON data as .ics data
 const ics = require('ics');
 
+// Function which turns JSON data into ics data
 module.exports = function JSONToICS(events, callback) {
-  ics.createEvents(events, function (err, output) {
+  ics.createEvents(events, function(err, output) {
     if (!err) {
       console.log(output);
+      // Sends ics data back to user
       callback(output);
     } else {
-      console.error(err);
+      // Throw error
       throw err;
     }
   });
--- routes/getSchedule.js
+++ routes/getSchedule.js
@@ -1,6 +1,9 @@
+// Adding request package
 const request = require('request');
 
+// Function for getting HTML data of schedule selected week
 module.exports = function getSchedule(username, password, week, csrf, cjar, callback) {
+  // Sending post request with login data, csrf code + cookies and the week
   request.post({
     url: config.rooster.baseUrl,
     jar: cjar,
@@ -11,8 +14,13 @@ module.exports = function getSchedule(username, password, week, csrf, cjar, call
       login: "loginform",
       weeknummer: week
     }
-  }, function (err, res, body) {
-    console.log(body);
-    callback(body);
+  }, function(err, res, body) {
+    if (!err) {
+      // Sends HTML data back
+      callback(body);
+    } else {
+      // Throws error
+      throw err;
+    }
   });
 }
